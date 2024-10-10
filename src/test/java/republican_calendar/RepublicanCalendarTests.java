ed21d72108e447d7f6c875946f26d721153c8546
marmothilde <mathilde.pirlet@gmail.com>
2024-10-10 13:26:18 +0200

    fix tests

--- src/test/java/republican_calendar/RepublicanCalendarTests.java
+++ src/test/java/republican_calendar/RepublicanCalendarTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import republican_calendar.exception.RepublicanCalendarException;
 import republican_calendar.types.RepublicanMonths;
 
 public class RepublicanCalendarTests {
@@ -28,13 +30,17 @@ public class RepublicanCalendarTests {
 
 	@Test
 	void instentiationTest() {
-		assertThrows(DataFormatException.class, () -> new RepublicanCalendar(1, RepublicanMonths.VENDEMIAIRE, 0));
-		assertThrows(DataFormatException.class, () -> new RepublicanCalendar(1, RepublicanMonths.VENDEMIAIRE, 31));
-		assertThrows(DataFormatException.class, () -> new RepublicanCalendar(1, null, 1));
-		assertThrows(DataFormatException.class, () -> new RepublicanCalendar(2, RepublicanMonths.SANSCULOTTIDES, 6));
-		assertThrows(DataFormatException.class, () -> new RepublicanCalendar(3, RepublicanMonths.SANSCULOTTIDES, 7));
+		assertThrows(RepublicanCalendarException.class,
+				() -> RepublicanCalendar.of(1, RepublicanMonths.VENDEMIAIRE, 0));
+		assertThrows(RepublicanCalendarException.class,
+				() -> RepublicanCalendar.of(1, RepublicanMonths.VENDEMIAIRE, 31));
+		assertThrows(RepublicanCalendarException.class, () -> RepublicanCalendar.of(1, null, 1));
+		assertThrows(RepublicanCalendarException.class,
+				() -> RepublicanCalendar.of(2, RepublicanMonths.SANSCULOTTIDES, 6));
+		assertThrows(RepublicanCalendarException.class,
+				() -> RepublicanCalendar.of(3, RepublicanMonths.SANSCULOTTIDES, 7));
 
-		assertDoesNotThrow(() -> new RepublicanCalendar(1, RepublicanMonths.VENDEMIAIRE, 1));
+		assertDoesNotThrow(() -> RepublicanCalendar.of(1, RepublicanMonths.VENDEMIAIRE, 1));
 	}
 
 	@Test
@@ -56,13 +62,17 @@ public class RepublicanCalendarTests {
 
 	@Test
 	void compareToTest() throws DataFormatException {
-		RepublicanCalendar date1 = new RepublicanCalendar(1, RepublicanMonths.VENDEMIAIRE, 1);
-		RepublicanCalendar date2 = new RepublicanCalendar(2, RepublicanMonths.SANSCULOTTIDES, 5);
-		RepublicanCalendar date3 = new RepublicanCalendar();
+		try {
+			RepublicanCalendar date1 = RepublicanCalendar.of(1, RepublicanMonths.VENDEMIAIRE, 1);
+			RepublicanCalendar date2 = RepublicanCalendar.of(2, RepublicanMonths.SANSCULOTTIDES, 5);
+			RepublicanCalendar date3 = new RepublicanCalendar();
 
-		assertEquals(0, date1.compareTo(date3));
-		assertTrue(date2.compareTo(date3) > 0);
-		assertTrue(date1.compareTo(date2) < 0);
+			assertEquals(0, date1.compareTo(date3));
+			assertTrue(date2.compareTo(date3) > 0);
+			assertTrue(date1.compareTo(date2) < 0);
+		} catch (RepublicanCalendarException e) {
+			fail();
+		}
 	}
 
 	@AfterEach
